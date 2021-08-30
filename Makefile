ea660b333b182f2aeb140d98fb15c3bc5b0da925
Hugh-J-Glas <willpower20005@gmail.com>
2021-08-30 22:51:04 +1000

    Further Testing Complteted some errors present

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CC = gcc
+CC = gcc -lm
 CFLAGS = -Wall -pedantic
 LDFLAGS =
 
--- dbl_vector.c
+++ dbl_vector.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "dbl_vector.h"
 
 #define DV_INITIAL_CAPACITY 4
@@ -23,21 +24,39 @@ void dv_ensure_capacity( dbl_vector_t* vec, size_t new_size )
     size_t old_capacity = vec->capacity;
     size_t old_size = vec->size;
     double* old_data = vec->data;
+    size_t new_mem_size = sizeof(double) * new_size;
     
-/**
- * WHERE old_capacity is the previous value of vec->capacity
- * AND old_size == previous value of vec->size
- * AND old_data == previous value of vec->data
- * AND new_capacity == max(old_capacity * DV_GROWTH_FACTOR, new_size)
- * AND new_mem_size == the number of bytes required to store new_capacity instances
- * of type double.
-**/
+    //This doesn't seem right
+    size_t new_cap = fmax(old_capacity*DV_GROWTH_FACTOR,new_size);
+    printf("TING 1: %fd \n", old_capacity*DV_GROWTH_FACTOR);
+    printf("TING 2: %ld \n",new_size);
+    vec->size = old_size;
+    bool Check2 = new_size <= vec->capacity;
+    printf("CHECK: %d\n",Check2);
 
+    if(Check2)
+    {
+        vec->capacity = old_capacity;
+        vec->data = old_data;
+    }
+    else
+    {
+        //Asign Values to struct
+        vec->capacity = new_cap;
+        vec->data = realloc(old_data,new_mem_size);
+        for(int i = 0; i < old_size; i++)
+        {
+            vec->data[i] = old_data[i];
+        }
+    }
 }
 
 void dv_destroy( dbl_vector_t* vec ) 
 {
-    // INSERT SOLUTION HERE
+    //Assign Information to be Zero
+    vec->size = 0;
+    vec->capacity=0;
+    free(vec->data);
 }
 
 void dv_copy( dbl_vector_t* vec, dbl_vector_t* dest ) 
@@ -47,35 +66,93 @@ void dv_copy( dbl_vector_t* vec, dbl_vector_t* dest )
 
 void dv_clear( dbl_vector_t* vec ) 
 {
-    // INSERT SOLUTION HERE
+    size_t OLD_Size = vec->size;
+    for(int i = 0; i < OLD_Size; i++)
+    {
+        vec->data[i] = 0;
+    }
 }
 
 void dv_push( dbl_vector_t* vec, double new_item ) 
 {
+    //Declare and Store Variables
     size_t OLD_Size = vec->size;
     double* OLD_Data = vec->data;
 
+    //Prepare for storage
     vec->size = OLD_Size+1;
     dv_ensure_capacity(vec,OLD_Size+1);
-    
+        
+    //Go through old data and reassign to vector
+    for(int i = 0; i < OLD_Size; i++)
+    {
+        vec->data[i] = OLD_Data[i];
+    }
+
+    vec->data[OLD_Size] = new_item;
 
 }
 
 void dv_pop( dbl_vector_t* vec ) 
 {
-    // INSERT SOLUTION HERE
+    size_t OLD_Size = vec->size;
+    double* OLD_Data = vec->data;
+
+    bool Check1 = OLD_Size > 0;
+
+    if(Check1)
+    {
+        vec->size = OLD_Size-1;
+        for(int i = 0; i < OLD_Size-1; i++)
+        {
+            vec->data[i] = OLD_Data[i];
+        }
+    }
+    else
+    {
+        vec->size=0;
+    }
+    
+
 }
 
 double dv_last( dbl_vector_t* vec ) 
 {
     double result = NAN;
+    
     // INSERT SOLUTION HERE
+
     return result;
 }
 
 void dv_insert_at( dbl_vector_t* vec, size_t pos, double new_item ) 
 {
-    // INSERT SOLUTION HERE
+    /**
+    size_t OLD_Size = vec->size;
+    double* OLD_Data = vec->data;
+    size_t OLD_Capacity = vec->capacity;
+    size_t loc = fmin(pos,OLD_Size);
+
+    vec->size = OLD_Size + 1;
+    dv_ensure_capacity(vec,OLD_Size+1);
+
+    for(int i = 0; i < (OLD_Size+1); i++)
+    {
+        if(i < loc)
+        {
+            vec->data[i] = OLD_Data[i];
+        }
+        else if(i == loc)
+        {
+            vec->data[i] = loc;
+        }
+        else
+        {
+            vec->data[i] = OLD_Data[i-1];
+        }
+    }
+    **/
+
 }
 
 void dv_remove_at( dbl_vector_t* vec, size_t pos ) 
